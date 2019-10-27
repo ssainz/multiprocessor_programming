054f4a5390aa8bd95ed2b28c5e0cd8953465ce4e
Sergio Sainz <sergio.sainz@gmail.com>
2019-10-27 14:08:06 -0400

    latest

--- hw4/part2_priorityQueue.sh
+++ hw4/part2_priorityQueue.sh
@@ -1,24 +1,27 @@
 echo "PriorityQueue"
+java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 2 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 4 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 8 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 16 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 32 64000
-java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 50 64000
-java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 64 64000
+#java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 50 64000
+#java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 64 64000
 echo "PriorityQueueTimeout"
+java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 2 64000 Timeout
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 4 64000 Timeout
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 8 64000 Timeout
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 16 64000 Timeout
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 32 64000 Timeout
-java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 50 64000 Timeout
-java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 64 64000 Timeout
+#java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 50 64000 Timeout
+#java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority PriorityQueueLock 64 64000 Timeout
 echo "CLH"
+java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 2 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 4 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 8 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 16 64000
 java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 32 64000
-java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 50 64000
-java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 64 64000
+#java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 50 64000
+#java -cp build/libs/hw4.jar edu.vt.ece.hw4.Benchmark normalPriority CLHLock 64 64000
 
 
 
