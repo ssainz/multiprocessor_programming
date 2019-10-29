f3131c027a234f2998b354b188c486b1c48b82d5
Sergio Sainz <sergio.sainz@gmail.com>
2019-10-28 22:15:10 -0400

    latest

--- hw4/src/main/java/edu/vt/ece/hw4/locks/SimpleHLock.java
+++ hw4/src/main/java/edu/vt/ece/hw4/locks/SimpleHLock.java
@@ -32,8 +32,8 @@ public class SimpleHLock implements Lock {
             //localLocks[i] = new SpinSleepLock(0,maxSpin); // Long waits in 128 cores.
             //localLocks[i] = new BackoffLock("Fixed"); // Longer than TAS
             //localLocks[i] = new CLHLock(); // About same as TAS
-            localLocks[i] = new MCSLock();
-            //localLocks[i] = new TTASLock(); // Somehow takes longer than TAS.
+            //localLocks[i] = new MCSLock(); // Pretty slow :P
+            localLocks[i] = new TTASLock(); // Somehow takes longer than TAS.
             //localLocks[i] = new TestAndSpinLock();
 
         }
