bcc400e71ba4a544097b13810f7c10e276308dfd
sergio sainz <ssainz@microstrategy.com>
2019-11-08 15:16:30 -0500

    latest

---
+++ hw5/set_evaluation.sh
@@ -0,0 +1,5 @@
+java -cp build/libs/hw5.jar edu.vt.ece.hw5.set.Benchmark  64000  CoarseList
+java -cp build/libs/hw5.jar edu.vt.ece.hw5.set.Benchmark  64000  FileList
+java -cp build/libs/hw5.jar edu.vt.ece.hw5.set.Benchmark  64000  LazyList
+java -cp build/libs/hw5.jar edu.vt.ece.hw5.set.Benchmark  64000  LockFreeList
+java -cp build/libs/hw5.jar edu.vt.ece.hw5.set.Benchmark  64000  OptimisticList
--- hw5/src/main/java/edu/vt/ece/hw5/set/Benchmark.java
+++ hw5/src/main/java/edu/vt/ece/hw5/set/Benchmark.java
@@ -7,73 +7,91 @@ public class Benchmark {
         // Fill this as you please, but you may be better off reusing code from
         // previous homeworks
 
-        final int threadCount = Integer.parseInt(args[0]);
-        final int iters = Integer.parseInt(args[1]);
-        final double percentageOfContains = Double.parseDouble(args[1]);
-        final String listType = args[3];
+        final int iters = Integer.parseInt(args[0]);
+
+        final String listType = args[1];
         //
-        ConcurrentSet set = null;
-        switch (listType){
-            case "CoarseList":
-                set = new CoarseList();
-                break;
-            case "FineList":
-                set = new FineList();
-                break;
-            case "LazyList":
-                set = new LazyList();
-                break;
-            case "LockFreeList":
-                set = new LockFreeList();
-                break;
-            case "OptimisticList":
-                set = new OptimisticList();
-                break;
-            default:
-                set = new CoarseList();
+        int[] threadsNumber = {4,9,14,19,20,24,29,35,40};
+        double[] pctg = {.2,.4,.6,.8};
+        //
+
+
+
+        for(int tC = 0 ; tC < threadsNumber.length ; tC++){
+
+            final int threadCount = threadsNumber[tC];
+
+            for(int pC = 0 ; pC < pctg.length ; pC++){
+
+                final double percentageOfContains = pctg[pC];
+
+                ConcurrentSet set = null;
+                switch (listType){
+                    case "CoarseList":
+                        set = new CoarseList();
+                        break;
+                    case "FineList":
+                        set = new FineList();
+                        break;
+                    case "LazyList":
+                        set = new LazyList();
+                        break;
+                    case "LockFreeList":
+                        set = new LockFreeList();
+                        break;
+                    case "OptimisticList":
+                        set = new OptimisticList();
+                        break;
+                    default:
+                        set = new CoarseList();
+                }
+
+                double[] throughputs = new double[7];
+                double[] times = new double[7];
+                for(int i = 0 ; i <= 6 ; i++){ // 7 times, first two times are warm up
+                    final SetTestThread[] threads = new SetTestThread[threadCount];
+
+                    for (int t = 0; t < threadCount; t++) {
+                        threads[t] = new SetTestThread(iters, set, percentageOfContains);
+                    }
+
+                    for (int t = 0; t < threadCount; t++) {
+                        threads[t].start();
+                    }
+
+                    long totalTime = 0;
+                    long maxTime = 0;
+                    for (int t = 0; t < threadCount; t++) {
+                        threads[t].join();
+                        totalTime += threads[t].getElapsedTime();
+                        maxTime = Math.max(maxTime, threads[t].getElapsedTime());
+
+
+                    }
+
+                    if(i > 1){
+                        throughputs[ i - 2 ] = (iters*threadCount) / (maxTime*0.001);
+                        times[ i - 2 ] = totalTime / threadCount;
+                    }
+                }
+
+
+                double totalThroughput = 0;
+                double totalTimeOverall = 0;
+                for(int i = 0 ; i < 5 ; i++ ){
+                    totalThroughput += throughputs[i];
+                    totalTimeOverall += times[i];
+                }
+                double avgThroughput = totalThroughput / 5;
+                double avgTime = totalTimeOverall / 5;
+
+
+                System.out.println(String.format("[%s\t][%d\t][%f\t][Avg.Throughput][Avg.Time]  %f \t %f", listType, threadCount , percentageOfContains, avgThroughput, avgTime) );
+
+            }
+
         }
 
-        double[] throughputs = new double[5];
-        double[] times = new double[5];
-        for(int i = 0 ; i <= 6 ; i++){ // 7 times, first two times are warm up
-            final SetTestThread[] threads = new SetTestThread[threadCount];
-
-            for (int t = 0; t < threadCount; t++) {
-                threads[t] = new SetTestThread(iters, set, percentageOfContains);
-            }
-
-            for (int t = 0; t < threadCount; t++) {
-                threads[t].start();
-            }
-
-            long totalTime = 0;
-            long maxTime = 0;
-            for (int t = 0; t < threadCount; t++) {
-                threads[t].join();
-                totalTime += threads[t].getElapsedTime();
-                maxTime = Math.max(maxTime, threads[t].getElapsedTime());
-
-
-            }
-
-            if(i > 1){
-                throughputs[ i - 2 ] = (iters*threadCount) / (maxTime*0.001);
-                times[ i - 2 ] = totalTime / threadCount;
-            }
-        }
-
-
-        double totalThroughput = 0;
-        double totalTimeOverall = 0;
-        for(int i = 0 ; i < 5 ; i++ ){
-            totalThroughput += throughputs[i];
-            totalTimeOverall += times[i];
-        }
-        double avgThroughput = totalThroughput / 5;
-        double avgTime = totalTimeOverall / 5;
-
-
-        System.out.println(String.format("[%s\t][Avg.Througput][Avg.Time]  %d \t %d",listType, avgThroughput, avgTime) );
 
         //System.out.println("Bag: Average time per thread is " + totalTime / threadCount + "ms");
 
