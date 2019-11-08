41124c8732f1fa7ccaca5cf32ad7f4d054bdf7f6
sergio sainz <ssainz@microstrategy.com>
2019-11-08 15:20:25 -0500

    latest

---
+++ hw5/src/main/java/edu/vt/ece/hw5/bag/LockFreeBag.java
@@ -0,0 +1,9 @@
+package edu.vt.ece.hw5.bag;
+
+public class LockFreeBag<I extends Number> {
+    public void add(I i) {
+    }
+
+    public void tryRemoveAny() {
+    }
+}
---
+++ hw5/src/main/java/edu/vt/ece/hw5/bag/LockFreeList.java
@@ -0,0 +1,9 @@
+package edu.vt.ece.hw5.bag;
+
+public class LockFreeList<T> {
+    public void add(T rand) {
+    }
+
+    public void remove(T i) {
+    }
+}
